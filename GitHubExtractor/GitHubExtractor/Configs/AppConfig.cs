3fd1e44446d0e47f8d91dbcc37da96566e06ec29
Felipe Félix dos Santos <felipefelixsantos@gmail.com>
2021-04-21 15:59:43 -0300

    Create ConfigFile and ConfigReader

---
+++ GitHubExtractor/GitHubExtractor/Configs/AppConfig.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace GitHubExtractor.Configs
+{
+	public class AppConfig
+	{
+		private static AppConfig _instance;
+		public static AppConfig Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = new AppConfig();
+				}
+				return _instance;
+			}
+			private set
+			{
+				//does nothing
+			}
+		}
+
+		private IConfiguration Configuration { get; set; }
+
+		private AppConfig()
+		{
+			IConfiguration configuration = new ConfigurationBuilder()
+				.AddJsonFile("Configs/appsettings.json", optional: false, reloadOnChange: true)
+				.Build();
+
+			Configuration = configuration;
+		}
+
+		public string GetConfig(string key)
+		{
+			string config = Configuration[key];
+			return config;
+		}
+	}
+}
