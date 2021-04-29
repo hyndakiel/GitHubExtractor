e5b20e86d8a77087596082dd615884172b088d64
Felipe Félix dos Santos <felipefelixsantos@gmail.com>
2021-04-29 17:29:40 -0300

    Remove body paramn

--- GitHubExtractor/GitHubExtractor/Dtos/PullRequestParamns.cs
+++ GitHubExtractor/GitHubExtractor/Dtos/PullRequestParamns.cs
@@ -8,17 +8,14 @@ namespace GitHubExtractor.Dtos
 		{
 		}
 
-		public PullRequestParamns(string state, string @base, string sort)
+		public PullRequestParamns(string state, string sort)
 		{
 			State = state;
-			Base = @base;
 			Sort = sort;
 		}
 
 		[JsonProperty("state")]
 		public string State { get; set; }
-		[JsonProperty("base")]
-		public string Base { get; set; }
 		[JsonProperty("sort")]
 		public string Sort { get; set; }
 	}
