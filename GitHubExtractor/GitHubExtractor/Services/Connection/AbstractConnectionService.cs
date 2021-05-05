914c4a5c555d2f0b2c458d291ec55acbc44e37dc
Felipe Félix dos Santos <felipefelixsantos@gmail.com>
2021-05-05 19:48:43 -0300

    Change authorization method

--- GitHubExtractor/GitHubExtractor/Services/Connection/AbstractConnectionService.cs
+++ GitHubExtractor/GitHubExtractor/Services/Connection/AbstractConnectionService.cs
@@ -103,9 +103,8 @@ namespace GitHubExtractor.Services.Connection
 			{
 				Tuple<string, string>[] headers = new Tuple<string, string>[]
 				{
-					//new Tuple<string, string>("Authorization", auth.Authorization),
-					new Tuple<string, string>("TOKEN", auth.Token),
 					new Tuple<string, string>("User-Agent", auth.Authorization),
+					new Tuple<string, string>("Authorization", "Bearer " + auth.Token),
 				};
 
 				string response;
